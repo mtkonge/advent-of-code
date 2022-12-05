f391f78140bbb78dd86582de0f071165e01544ac
Mikkel Troels Kongsted <mtkongsted@gmail.com>
2022-12-05 17:27:37 +0100

    day1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # advent-of-code
+
+an advent for coding challenges to 25th of december
+
+https://adventofcode.com/2022/
---
+++ day1/main.py
@@ -0,0 +1,32 @@
+# velkommen til shit code centrallen
+
+def main():
+    calories = open("input.txt", "r").read()
+    array = calories.split("\n")
+    currentValue = 0
+    newArray = []
+    for v in array:
+        if v == "":
+            newArray.append(currentValue)
+            currentValue = 0
+        else: 
+            currentValue += int(v)
+    index = 0
+    top3Values = []
+    for v in range(3):
+        for i in range(len(newArray)):
+            if newArray[i] > currentValue:
+                print(currentValue)
+                currentValue = newArray[i]
+                index = i
+        top3Values.append(currentValue)
+        currentValue = 0
+        newArray.pop(index)
+    result = 0
+    for v in top3Values:
+        result += v
+    print(result)
+    
+
+if __name__ == "__main__":
+    main()
