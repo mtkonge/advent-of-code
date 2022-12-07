2ca61c55783c7400a837b3b85fe30bad0f60fe5d
Mikkel <mtkongsted@gmail.com>
2022-12-07 10:26:04 +0100

    gave up on day 3 part two for now

--- day3/main.py
+++ day3/main.py
@@ -2,6 +2,40 @@ def main():
     backpacks = open("input.txt", "r").read().split("\n")
     characters = "abcdefghijklmnopqrstuvwxyz"
     characters += characters.upper()
+    partTwo(backpacks, characters)
+
+
+def partTwo(backpacks, characters, i = 0):
+    badges = ""
+    for i in range(len(backpacks)):
+        if i == 0:
+            i += 1
+            continue
+        elif not (i % 3):
+            currentDuplicates = ""
+            for v in backpacks[i-2]:
+                for s in backpacks[i-1]:
+                    if v == s:
+                        if len(currentDuplicates) == 0:
+                            currentDuplicates += v
+                        for i in range(len(currentDuplicates)):
+                            if currentDuplicates[i] == v:
+                                break;
+                            elif i == len(currentDuplicates)-1:
+                                currentDuplicates += v
+            for c in currentDuplicates:
+                for d in backpacks[i]:
+                    if c == d:
+                        badges += c
+                        break
+    result = 0
+    for v in badges:
+        result += characters.index(v)+1
+    print("part two:")
+    print(result)
+    print("not completed")
+
+def partOne(backpacks, characters):
     for i in range(len(backpacks)):
         x = backpacks[i][0:int(len(backpacks[i])/2)]
         y = backpacks[i][int(len(backpacks[i])/2):len(backpacks[i])]
@@ -12,8 +46,6 @@ def main():
         result += characters.index(v)+1
     print("part one:")
     print(result)
-
-    
 
 def itemsToRearrange(backpacks, dups = ""):
     for bp in backpacks:
